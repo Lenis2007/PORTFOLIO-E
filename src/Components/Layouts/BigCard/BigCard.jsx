ace0da4d356e790aea48af80d67867c00c53b372
Lenis2007 <lenisalfonsocastillo@gmail.com>
2025-03-17 22:56:24 -0500

    Cambio de responsive

--- src/Components/Layouts/BigCard/BigCard.jsx
+++ src/Components/Layouts/BigCard/BigCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const BigCard = ({ children }) => {
     return (
 
-        <div className='text-white py-12 container mx-auto px-1 space-y-4'>
+        <div className='text-white mx-auto px-1 space-y-4'>
             {children}
         </div>
 
