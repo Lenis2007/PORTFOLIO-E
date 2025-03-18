be648a824140f8f1e1b312ee9b1d22cf32e560c0
Lenis2007 <lenisalfonsocastillo@gmail.com>
2025-03-17 23:00:01 -0500

    Cambio de responsive

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -13,7 +13,7 @@ export const Home = () => {
       
       <div className='text-center lg:text-left max-w-lg'>
         <h1 className='text-4xl font-bold mb-4 text-[#FFC107]'>Hola, soy Lenis Alfonso</h1>
-        <p>
+        <p className='text-justify'>
           Soy una desarrolladora junior full stack web con formación en <span className='text-amber-200'>Tecnología en Análisis y Desarrollo de Software del SENA</span>, donde actualmente estoy cursando el tecnólogo tras haber completado el técnico en el colegio. Me apasiona adquirir nuevos conocimientos y mejorar mis habilidades para crecer profesionalmente. A pesar de que el aprendizaje puede ser un reto, me esfuerzo por encontrar métodos que faciliten mi comprensión. Valoro la oportunidad de participar en proyectos desafiantes, donde puedo contribuir activamente al éxito del equipo y reforzar mi desarrollo.
         </p>
       </div>
